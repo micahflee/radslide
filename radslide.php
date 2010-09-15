636479b4e2c0c61c06b9248f37a5670fa40caa0e
Micah Lee <twopointfour@riseup.net>
2010-09-15 16:58:04 -0700

    update plugin info, license

--- radslide.php
+++ radslide.php
@@ -1,13 +1,30 @@
 <?php
 /*
  * Plugin Name: radSLIDE
- * Plugin URI: http://www.radicaldesigns.org/
- * Description: A slideshow plugin, made specifically for radicalDESIGN's clients
+ * Plugin URI: http://radicaldesigns.org/wordpress-plugin-radslide
+ * Description: A javascript slideshow plugin that supports multiple slideshows and uses jQuery Cycle
  * Version: 1.0
  * Author: Micah Lee <micah@radicaldesigns.org>
- * Author URI: http://www.radicaldesigns.org/
+ * Author URI: http://github.com/micahflee
  * License: GPL2
  * */
+
+/*  Copyright 2010  RadicalDesigns
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 
 require_once('display.php');
 require_once('helpers.php');
