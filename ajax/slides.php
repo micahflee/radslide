b5728960a80df5256c87d2ab0ea180d3dc552296
Micah Lee <twopointfour@riseup.net>
2010-11-19 18:14:55 -0800

    displays the id in the slides page

--- ajax/slides.php
+++ ajax/slides.php
@@ -12,6 +12,7 @@ function radslide_ajax_slides_populate() {
 
   <table>
     <tr>
+			<th>ID</th>
       <th>Image</th>
       <th>Title</th>
       <th>Description</th>
@@ -26,6 +27,7 @@ function radslide_ajax_slides_populate() {
     foreach($rows as $row) {
       ?>
 			<tr class="radslide_row" id="radslide_row-<?php echo($row->id); ?>">
+				<td><?php echo($row->id); ?></td>
 				<td style="text-align:center">
 					<input type="hidden" class="radslide_field" id="radslide_update-image_url-<?php echo($row->id); ?>" value="<?php echo(stripslashes($row->image_url)); ?>" />
 					<?php if(!empty($row->image_url)) { ?><img src="<?php echo(stripslashes($row->image_url)); ?>" height="50" id="radslide_update-image-<?php echo($row->id); ?>" /><?php } ?>
@@ -37,19 +39,20 @@ function radslide_ajax_slides_populate() {
         <td style="text-align:center">
 					<input type="button" class="button-primary radslide_image_picker" id="radslide_image_picker-<?php echo($row->id); ?>" value="Choose Image" />
           <input type="submit" class="button-primary" value="Delete" id="radslide_delete-<?php echo($row->id); ?>" />
-        <td>
+        </td>
         <td><?php radslide_helper_ajax_loader("radslide_loading-".$row->id); ?></td>
       </tr>
       <?php
     }
     ?>
-    <tr><td colspan="6"><div style="background-color:#999999;height:1px;margin:8px 0;"></div></td></tr>
-    <tr id="radslide_add_row">
+    <tr><td colspan="8"><div style="background-color:#999999;height:1px;margin:8px 0;"></div></td></tr>
+		<tr id="radslide_add_row">
+			<td></td>
  			<td style="text-align:center">
 				<input type="hidden" id="radslide_add-image_url" value="" />
 				<img src="" width="100" id="radslide_add-image" />
 			</td>
-     <td><input type="text" id="radslide_add-title" value="Enter title" /></td>
+      <td><input type="text" id="radslide_add-title" value="Enter title" /></td>
       <td><input type="text" id="radslide_add-description" value="Enter description" /></td>
       <td><input type="text" id="radslide_add-link_url" value="http://" /></td>
       <td><input type="text" style="width:3em;" id="radslide_add-sort" value="0" /></td>
@@ -57,7 +60,7 @@ function radslide_ajax_slides_populate() {
 				<input type="button" class="button-primary radslide_image_picker" id="radslide_image_picker-add" value="Choose Image" />
         <input type="button" class="button-primary" value="Add Slide" id="radslide_add" />
       </td>
-      <td><?php radslide_helper_ajax_loader("radslide_loading"); ?></td>
+			<td><?php radslide_helper_ajax_loader("radslide_loading"); ?></td>
     </tr>
 	</table>
   <p><input type="button" class="button-primary" value="Save Changes" id="radslide_update" /></p>
